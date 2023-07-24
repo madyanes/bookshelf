6172ae40e617cd66102e700bebfe50839fd743e0
madyanes <madyanes.dev@gmail.com>
2023-07-24 18:19:25 +0700

    show books in document

--- bookshelf.js
+++ bookshelf.js
@@ -4,12 +4,15 @@
 const books = []
 const STORAGE_KEY = 'BOOK_APP'
 const EVENT_STORED = 'stored-book'
+const EVENT_RENDER = 'render-book'
 
 /**
  * Fungsi-fungsi yang akan dijalankan setelah dokumen HTML dimuat
  */
 document.addEventListener('DOMContentLoaded', () => {
-    isStorageExist()
+    if (isStorageExist()) {
+        loadDataFromStorage()
+    }
 
     const bookForm = document.querySelector('form')
     bookForm.addEventListener('submit', (e) => {
@@ -24,6 +27,20 @@ document.addEventListener('DOMContentLoaded', () => {
  */
 document.addEventListener(EVENT_STORED, () => {
     console.info(localStorage.getItem(STORAGE_KEY))
+})
+
+
+/**
+ * Kejadian kustom; Tampilkan semua buku di dokumen
+ */
+document.addEventListener(EVENT_RENDER, () => {
+    const unreadBookElement = document.querySelector('#unread .book-item-container')
+    unreadBookElement.innerHTML = ''
+
+    for (const book of books) {
+        const bookItemElement = createBookItemElement(book)
+        unreadBookElement.innerHTML += bookItemElement
+    }
 })
 
 
@@ -81,6 +98,7 @@ const addBook = () => {
     const bookObject = generateBookOject(bookID, bookTitle, bookAuthor, bookYear, false)
     books.push(bookObject)
 
+    document.dispatchEvent(new Event(EVENT_RENDER))
     storeBook()
 }
 
@@ -94,4 +112,42 @@ const storeBook = () => {
         localStorage.setItem(STORAGE_KEY, bookArrayObjectToString)
         document.dispatchEvent(new Event(EVENT_STORED))
     }
+}
+
+
+/**
+ * 
+ * @param {Object} bookObject data buku dalam bentuk objek
+ * @returns Susunan elemen tentang item buku dalam bentuk string
+ */
+const createBookItemElement = (bookObject) => {
+    const divBookItem = `
+        <div class="book-item" id="#">
+            <img src="img/book-cover-placeholder.png" alt="placeholder">
+            <div class="book-detail">
+                <h3>${bookObject.title}</h3>
+                <p>by ${bookObject.author}</p>
+                <p>${bookObject.year}</p>
+            </div>
+        </div>
+    `
+
+    return divBookItem
+}
+
+
+/**
+ * Masukkan semua data (data buku) ke dalam array buku
+ */
+const loadDataFromStorage = () => {
+    const dataAsString = localStorage.getItem(STORAGE_KEY)
+    const dataAsObject = JSON.parse(dataAsString)
+
+    if (dataAsObject !== null) {
+        for (const book of dataAsObject) {
+            books.push(book)
+        }
+    }
+
+    document.dispatchEvent(new Event(EVENT_RENDER))
 }
