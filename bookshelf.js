faa65ae8adb2fddf3604e55aa29d3bb43163aac9
madyanes <madyanes.dev@gmail.com>
2023-07-24 15:32:14 +0700

    book object blueprint

--- bookshelf.js
+++ bookshelf.js
@@ -1,4 +1,9 @@
 /**
+ * Global variables
+ */
+const books = []
+
+/**
  * Fungsi-fungsi yang akan dijalankan setelah dokumen HTML dimuat
  */
 document.addEventListener('DOMContentLoaded', () => {
@@ -15,4 +20,33 @@ const isStorageExist = () => {
         return false
     }
     return true
+}
+
+
+/**
+ * Generator timestamp
+ * @returns timestamp / current number of miliseconds (unix epoch)
+ */
+const generateId = () => {
+    return +new Date()
+}
+
+
+/**
+ * Generator objek buku
+ * @param {Number} id ID buku
+ * @param {String} title Judul buku
+ * @param {String} author Penulis
+ * @param {Number} year Tahun terbit
+ * @param {Boolean} isComplete Status baca
+ * @returns Objek buku
+ */
+const generateBookOject = (id, title, author, year, isComplete) => {
+    return {
+        id,
+        title,
+        author,
+        year,
+        isComplete
+    }
 }
